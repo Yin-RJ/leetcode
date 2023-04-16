89733b5f8bbaa1070e7c3973dbb515260f257e3d
YinRJ <yin_rj@qq.com>
2023-04-16 13:23:39 +0800

    problem 1023

---
+++ src/com/yinrj/leetcode/editor/cn/[1023]驼峰式匹配.java
@@ -0,0 +1,83 @@
+package com.yinrj.leetcode.editor.cn;//如果我们可以将小写字母插入模式串 pattern 得到待查询项 query，那么待查询项与给定模式串匹配。（我们可以在任何位置插入每个字符，也可以插入 0
+//个字符。） 
+//
+// 给定待查询列表 queries，和模式串 pattern，返回由布尔值组成的答案列表 answer。只有在待查项 queries[i] 与模式串 
+//pattern 匹配时， answer[i] 才为 true，否则为 false。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+// pattern = "FB"
+//输出：[true,false,true,true,false]
+//示例：
+//"FooBar" 可以这样生成："F" + "oo" + "B" + "ar"。
+//"FootBall" 可以这样生成："F" + "oot" + "B" + "all".
+//"FrameBuffer" 可以这样生成："F" + "rame" + "B" + "uffer". 
+//
+// 示例 2： 
+//
+// 
+//输入：queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+// pattern = "FoBa"
+//输出：[true,false,true,false,false]
+//解释：
+//"FooBar" 可以这样生成："Fo" + "o" + "Ba" + "r".
+//"FootBall" 可以这样生成："Fo" + "ot" + "Ba" + "ll".
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+// pattern = "FoBaT"
+//输出：[false,true,false,false,false]
+//解释： 
+//"FooBarTest" 可以这样生成："Fo" + "o" + "Ba" + "r" + "T" + "est".
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= queries.length <= 100 
+// 1 <= queries[i].length <= 100 
+// 1 <= pattern.length <= 100 
+// 所有字符串都仅由大写和小写英文字母组成。 
+// 
+//
+// Related Topics 字典树 双指针 字符串 字符串匹配 👍 109 👎 0
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution1023 {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        List<Boolean> res = new ArrayList<>();
+
+        for (String query : queries) {
+            res.add(check(query, pattern));
+        }
+
+        return res;
+    }
+
+    private static boolean check(String query, String pattern) {
+        int i = 0;
+        int n = pattern.length();
+        for (char ch : query.toCharArray()) {
+            if (i < n && ch == pattern.charAt(i)) {
+                i++;
+            } else if (ch < 'a') {
+                return false;
+            }
+        }
+        return n == i;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
